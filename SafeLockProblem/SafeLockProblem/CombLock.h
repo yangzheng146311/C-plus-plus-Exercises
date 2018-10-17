5d4b076c26ef4d0cae5dd27d2a51e2685d3d2a4a
yangzheng146311 <36888195+yangzheng146311@users.noreply.github.com>
2018-10-17 14:53:18 +0100

    slight change

--- SafeLockProblem/SafeLockProblem/CombLock.h
+++ SafeLockProblem/SafeLockProblem/CombLock.h
@@ -15,28 +15,27 @@ private:
 	static int UHF[4];
 	static int LHF[4];
 	static int PHF[4];
+
+	static int a;
 	
 	int id = -1;
 	int CN[4] = { 0,0,0,0 };
 	int LN[4] = { 0,0,0,0 };
 	int NH[4] = { 0,0,0,0 };
 
-
+	int Turn(int x, int y);
+	bool CheckCN();
 	static void Generate_ROOT();
 	static void Generate_UHF();
 	static void Generate_LHF();
 	static void Generate_PHF();
 
 public:
+	static int b;
 	CombLock();//First Lock Constructor
 	CombLock(const CombLock &comblock);//After Lock Constructor
 	~CombLock();
-
-	//static FILE myFile;
-	static std::ofstream myfile;
-
-
+	static ofstream myfile;
 	static bool Build_SafeLock();
-	int Turn(int x,int y);
-	bool CheckCN();
+	
 };
